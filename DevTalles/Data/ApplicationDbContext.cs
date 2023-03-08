50eaf8aa33345708f8c46e61a634275bbb85eea4
Jadan Cuadra <jadancuadra1@gmail.com>
2023-03-07 21:54:36 -0600

    Add project files.

---
+++ DevTalles/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using E_comerce.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_comerce.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Productos> Producto { get; set; }
+       
+    }
+}
